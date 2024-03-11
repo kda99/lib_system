2607cb2f06443ce03f452fc4ad041ecb241fff6f
Дмитрий Капустин <kda99@mail.ru>
2024-03-11 20:04:05 +0300

    1

--- lib_system/lib_system/urls.py
+++ lib_system/lib_system/urls.py
@@ -19,9 +19,20 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+from organizations.views import OrganizationViewSet
+from events.views import EventViewSet
+
+router = DefaultRouter()
+router.register(r'organizations', OrganizationViewSet)
+router.register(r'events', EventViewSet)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls)),
 ]
--- lib_system/organizations/views.py
+++ lib_system/organizations/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Organization
+from .serializers import OrganizationSerializer
 
-# Create your views here.
+class OrganizationViewSet(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
